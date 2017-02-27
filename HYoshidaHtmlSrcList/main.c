3d0d2346dc8c86628cebabaf3eacae61ff95c801
Yoshida Hideaki <hidey@hideaki-no-macbook-pro.local>
2017-02-26 19:10:39 -0800

    Initial Commit

---
+++ HYoshidaHtmlSrcList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HYoshidaHtmlSrcList
+//
+//  Created by Hideaki Yoshida on 2017/02/26.
+//  Copyright © 2017年 Hideaki Yoshida. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
